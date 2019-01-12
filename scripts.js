7e8d8c770c0d0f83922e309d6ecab438ba785ac2
Craig Gorton <ncraiggorton@gmail.com>
2019-01-12 09:57:10 -0500

    clean up working version

--- scripts.js
+++ scripts.js
@@ -3,22 +3,13 @@ var possibilities = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 var board = [];
 
 function step() {
-    checkIfDone();
-
     deleteListsIfAlreadySolved();
-    drawBoard();
-
     checkRows();
-    drawBoard();
-
     checkColumns();
-    drawBoard();
-
     checkSubGrids();
-    drawBoard();
-
     checkForAnswers();
     drawBoard();
+    checkIfDone();
 }
 
 function checkIfDone() {
@@ -34,6 +25,7 @@ function checkIfDone() {
         alert("You're done!");
     }
 }
+
 function deleteListsIfAlreadySolved() {
     for (var y = 0; y < 9; y++) {
         for (var x = 0; x < 9; x++) {
@@ -54,6 +46,7 @@ function checkForAnswers() {
     }
 }
 
+// For each cell, remove from possible list any values found in that row
 function checkRows() {
     for (var y = 0; y < 9; y++) {
         for (var x = 0; x < 9; x++) {
@@ -64,6 +57,7 @@ function checkRows() {
     }
 }
 
+// For each cell, remove from possible list any values found in that column
 function checkColumns() {
     for (var y = 0; y < 9; y++) {
         for (var x = 0; x < 9; x++) {
@@ -74,6 +68,7 @@ function checkColumns() {
     }
 }
 
+// For each cell, remove from possible list any values found in that sub-grid
 function checkSubGrids() {
     for (var y = 0; y < 9; y++) {
         for (var x = 0; x < 9; x++) {
@@ -84,15 +79,16 @@ function checkSubGrids() {
     }
 }
 
+// For a given cell, return a list of candidates, having pruned any that are already found in the same sub-grid
 function pruneBySubGrid(y, x) {
     var tempList = Array.from(board[y][x].list);
     var upperLeftX = Math.floor(x / 3) * 3;
     var upperLeftY = Math.floor(y / 3) * 3;
 
-    // foreach list item
+    // for each candidate left on the list
     for (var i = 0; i < board[y][x].list.length; i++) {
         var listItemValue = board[y][x].list[i];
-        // check all sub-grids for answers
+        // check all sub-grid cells for answers
         for (var sgy = upperLeftY; sgy < upperLeftY + 3; sgy++) {
             for (var sgx = upperLeftX; sgx < upperLeftX + 3; sgx++) {
                 var answerToCheck = board[sgy][sgx].answer;
@@ -106,6 +102,7 @@ function pruneBySubGrid(y, x) {
     return tempList;
 }
 
+// For a given cell, return a list of candidates, having pruned any that are already found in the same row
 function pruneByRow(y, x) {
     var tempList = Array.from(board[y][x].list);
     for (var i = 0; i < 9; i++) {
@@ -121,6 +118,7 @@ function pruneByRow(y, x) {
     return tempList;
 }
 
+// For a given cell, return a list of candidates, having pruned any that are already found in the same column
 function pruneByColumn(y, x) {
     var tempList = Array.from(board[y][x].list);
     for (var i = 0; i < 9; i++) {
@@ -138,11 +136,11 @@ function pruneByColumn(y, x) {
 
 function initSudoku() {
     generateBoardHtml();
-    initBoard();
+    initBoardData();
     drawBoard();
 }
 
-function initBoard() {
+function initBoardData() {
     for (var y = 0; y < 9; y++) {
         var cellList = [];
         for (var x = 0; x < 9; x++) {
@@ -151,7 +149,10 @@ function initBoard() {
         }
         board.push(cellList);
     }
-    loadSimpleBoard();
+
+    //loadSimpleBoard();
+    loadMediumBoard();
+
     deleteListsIfAlreadySolved();
 }
 
@@ -193,11 +194,47 @@ function loadSimpleBoard() {
     board[8][7].answer = 6;
 }
 
+function loadMediumBoard() {
+    board[0][3].answer = 4;
+    board[0][7].answer = 5;
+
+    board[1][3].answer = 7;
+    board[1][5].answer = 5;
+    board[1][8].answer = 4;
+
+    board[2][1].answer = 5;
+    board[2][4].answer = 1;
+    board[2][7].answer = 9;
+
+    board[3][0].answer = 1;
+    board[3][1].answer = 4;
+    board[3][2].answer = 5;
+    board[3][4].answer = 2;
+    board[3][7].answer = 3;
+
+    board[4][4].answer = 9;
+
+    board[5][0].answer = 2;
+    board[5][2].answer = 6;
+    board[5][6].answer = 4;
+
+    board[6][0].answer = 4;
+    board[6][2].answer = 2;
+    board[6][6].answer = 3;
+
+    board[7][0].answer = 8;
+    board[7][1].answer = 7;
+    board[7][3].answer = 1;
+
+    board[8][4].answer = 3;
+    board[8][8].answer = 1;
+}
+
 function drawBoard() {
     for (var tr = 0; tr < 9; tr++) {
         for (var td = 0; td < 9; td++) {
             var id = tr.toString() + td.toString();
-            //document.getElementById(id).innerHTML = board[tr][td].list + "<br/><strong>" + board[tr][td].answer + "</strong>";
+            document.getElementById(id).innerHTML = board[tr][td].list + "<br/><strong>" + board[tr][td].answer + "</strong>";
             if (board[tr][td].answer != 0) {
                 document.getElementById(id).innerHTML = "<strong>" + board[tr][td].answer + "</strong>";
                 document.getElementById(id).className = "table-success";
@@ -222,10 +259,10 @@ function generateBoardHtml() {
 
     document.getElementById("grid").innerHTML = gridHtml;
 
-    setBorders();
+    drawSubGridBorders();
 }
 
-function setBorders() {
+function drawSubGridBorders() {
     for (var tr = 0; tr < 9; tr++) {
         for (var td = 0; td < 9; td++) {
             if (tr == 2 || tr == 5) {
